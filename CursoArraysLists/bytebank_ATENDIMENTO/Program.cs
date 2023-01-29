e052d23f2de0092914b875d661661e8fac7ff616
tarcisiokta2012@gmail.com <tarcisiokta2012@gmail.com>
2023-01-29 18:09:43 -0300

    Array de Palavras

--- CursoArraysLists/bytebank_ATENDIMENTO/Program.cs
+++ CursoArraysLists/bytebank_ATENDIMENTO/Program.cs
@@ -1,21 +1,52 @@
 ﻿Console.WriteLine("Boas Vindas ao ByteBank, Atendimento.");
 
-int indice =0;
-int acumulador = 0;
-int[] idades = new int[5];
+TestaBuscarPalavras();
 
-idades[0] = 10;
-idades[1] = 20;
-idades[2] = 30;
-idades[3] = 40;
-idades[4] = 50;
-
-foreach (var idade in idades)
+void TestaBuscarPalavras()
 {
-    System.Console.WriteLine($"Indice{indice} = {idade}");
-    indice++;
-    acumulador+= idade;
+    string[] palavras = new string[5];
+
+    for (int i = 0; i < palavras.Length; i++)
+    {
+        System.Console.Write($"Digite {i+1}° Palavra : ");
+        palavras[i] = Console.ReadLine();
+    }
+
+    System.Console.Write("Digite Palavra a Ser Buscada : ");
+    string palavraProcurada = Console.ReadLine();
+
+    foreach (var palavra in palavras)
+    {
+        if(palavra.Equals(palavraProcurada))
+        {
+            System.Console.WriteLine($"Palavra {palavra} Encontrada");
+            return;
+        }
+    }
+    System.Console.WriteLine("Palavra Não Encontrada");
+
 }
 
-System.Console.WriteLine($"Média de Idade {acumulador/idades.Length}");
+void Idades()
+{
+    int indice =0;
+    int acumulador = 0;
+    int[] idades = new int[5];
+
+    idades[0] = 10;
+    idades[1] = 20;
+    idades[2] = 30;
+    idades[3] = 40;
+    idades[4] = 50;
+
+    foreach (var idade in idades)
+    {
+        System.Console.WriteLine($"Indice{indice} = {idade}");
+        indice++;
+        acumulador+= idade;
+    }
+
+    System.Console.WriteLine($"Média de Idade {acumulador/idades.Length}");
+}
+
 
