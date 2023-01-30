7dc364efa53f54e3c44824a7834eb86dd42672ea
tarcisiokta2012@gmail.com <tarcisiokta2012@gmail.com>
2023-01-30 16:06:03 -0300

    A Classe Array

--- CursoArraysLists/bytebank_ATENDIMENTO/Program.cs
+++ CursoArraysLists/bytebank_ATENDIMENTO/Program.cs
@@ -1,6 +1,6 @@
 ﻿Console.WriteLine("Boas Vindas ao ByteBank, Atendimento.");
 
-TestaBuscarPalavras();
+
 
 void TestaBuscarPalavras()
 {
@@ -49,4 +49,24 @@ void Idades()
     System.Console.WriteLine($"Média de Idade {acumulador/idades.Length}");
 }
 
+Array amostra = Array.CreateInstance(typeof(double),5);
 
+amostra.SetValue(5.9,0);
+amostra.SetValue(1.8,1);
+amostra.SetValue(7.1,2);
+amostra.SetValue(10,3);
+amostra.SetValue(6.9,4);
+
+void testaMediana(Array array)
+{
+    if(array == null || array.Length == 0) return;
+
+    double[] numerosOrdenados = (double[])array.Clone();
+    Array.Sort(numerosOrdenados);
+
+    int tamanho = numerosOrdenados.Length;
+    int meio = tamanho/2;
+
+    double mediana = (tamanho % 2 != 0) ? numerosOrdenados[meio] :
+    (numerosOrdenados[meio] +  numerosOrdenados[meio -1])/2;
+}
