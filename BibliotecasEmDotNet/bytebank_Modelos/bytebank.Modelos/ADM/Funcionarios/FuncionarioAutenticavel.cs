83ec453c0ab0ca6e8dae677ab94daa84405f0377
tarcisiokta2012@gmail.com <tarcisiokta2012@gmail.com>
2023-03-20 16:20:07 -0300

    Iniciando Curso DotNetMongoDB

--- BibliotecasEmDotNet/bytebank_Modelos/bytebank.Modelos/ADM/Funcionarios/FuncionarioAutenticavel.cs
+++ BibliotecasEmDotNet/bytebank_Modelos/bytebank.Modelos/ADM/Funcionarios/FuncionarioAutenticavel.cs
@@ -1,21 +1,23 @@
 ﻿using bytebank.Modelos.ADM.SistemaInterno;
-
+using bytebank_Modelos.bytebank.Modelos.ADM.Utilitario;
 
 namespace bytebank.Modelos.ADM.Funcionarios
 {
     public abstract class FuncionarioAutenticavel : Funcionario, IAutenticavel
     {
         public string Senha { get; set; }
-
+        public AutenticacaoUtil autenticaUtil { get; set; }
         public FuncionarioAutenticavel(double salario, string cpf)
             : base(salario, cpf)
         {
 
         }
 
+        
+
         public bool Autenticar(string senha)
         {
-            return Senha == senha;
+            return autenticaUtil.ValidarSenha(Senha, senha);
         }
     }
 }
--- BibliotecasEmDotNet/bytebank_Modelos/bytebank.Modelos/ADM/ParceriaComercial/ParceiroComercial.cs
+++ BibliotecasEmDotNet/bytebank_Modelos/bytebank.Modelos/ADM/ParceriaComercial/ParceiroComercial.cs
@@ -1,13 +1,17 @@
 ﻿using bytebank.Modelos.ADM.SistemaInterno;
+using bytebank_Modelos.bytebank.Modelos.ADM.Utilitario;
 
 namespace bytebank.Modelos.ADM.Utilitario
 {
     public class ParceiroComercial : IAutenticavel
     {
         public string Senha { get; set; }
+
+        public AutenticacaoUtil autenticaUtil { get; set; }
+
         public bool Autenticar(string senha)
         {
-            return Senha == senha;
+            return autenticaUtil.ValidarSenha(Senha, senha);
         }
     }
 }
