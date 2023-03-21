162e7fc33aa8abb54ad68e95f30893ead61d2b7e
tarcisiokta2012@gmail.com <tarcisiokta2012@gmail.com>
2023-03-20 21:06:50 -0300

    Acessando MongoDb

---
+++ CursoDotNetMongoDb/exemplosMongoDB/exemplosMongoDB/AcessandoMongoDB.cs
@@ -0,0 +1,36 @@
+﻿using MongoDB.Bson;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exemplosMongoDB
+{
+    internal class AcessandoMongoDB
+    {
+        public string ConnectionString { get; set; }
+        public IMongoClient Client { get; set; }
+        public IMongoDatabase DataBase { get; set; }
+        public IMongoCollection<BsonDocument> Colecao { get; set; }
+
+        public AcessandoMongoDB()
+        {
+            ConnectionString = "mongodb://localhost:27017";
+
+            Client = new MongoClient(ConnectionString);
+
+            DataBase = Client.GetDatabase("Biblioteca");
+
+            Colecao = DataBase.GetCollection<BsonDocument>("Livros");
+        }
+
+        public async Task InsertOnMongoDB(BsonDocument doc)
+        {
+            Colecao.InsertOneAsync(doc);
+            Console.WriteLine("Documento Incluido!");
+            Console.ReadLine();
+        }
+    }
+}
--- CursoDotNetMongoDb/exemplosMongoDB/exemplosMongoDB/Program.cs
+++ CursoDotNetMongoDb/exemplosMongoDB/exemplosMongoDB/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using MongoDB.Bson;
+using System;
+using System.Threading.Tasks;
 
 namespace exemplosMongoDB
 {
@@ -6,7 +8,24 @@ namespace exemplosMongoDB
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            MainAsync();
+        }
+        static async Task MainAsync()
+        {
+            var doc = new BsonDocument()
+            {
+                {"Titulo","A Guerra Dos Tronos"}
+            };
+            doc.Add("Autor", "Geoger R R Martin");
+            doc.Add("Ano", 1999);
+            doc.Add("Páginas", 856);
+            var assuntoArray = new BsonArray();
+            assuntoArray.Add("Ação");
+            assuntoArray.Add("Fantasia");
+            doc.Add("Assunto", assuntoArray);
+
+            AcessandoMongoDB acessandoMongoDB = new AcessandoMongoDB();
+            acessandoMongoDB.InsertOnMongoDB(doc);
         }
     }
 }
